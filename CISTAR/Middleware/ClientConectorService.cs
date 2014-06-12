a95098152c2c93897a798ae81a01c170a85a8f19
Johan Benschop <Joh739@gmail.com>
2014-06-12 21:31:37 +0200

    Added emty Signleton class. Temporary names.

---
+++ CISTAR/Middleware/ClientConectorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CISTAR.Middleware
+{
+    public class ClientConectorService
+    {
+        private static readonly Lazy<ClientConectorService> Lazy = new Lazy<ClientConectorService>(() => new ClientConectorService());
+        public static ClientConectorService Instance { get { return Lazy.Value; } }
+
+
+
+    }
+}
