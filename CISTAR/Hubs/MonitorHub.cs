59e3a892142046b5df9371103bc76433632470a8
Johan Benschop <Joh739@gmail.com>
2014-06-12 21:20:27 +0200

    Added two empty SignalR Hub classes.

---
+++ CISTAR/Hubs/MonitorHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CISTAR.Hubs
+{
+    public class MonitorHub : Hub
+    {
+        public void Hello()
+        {
+            Clients.All.hello();
+        }
+    }
+}
