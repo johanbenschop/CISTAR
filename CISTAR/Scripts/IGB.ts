4ced9254a9c7425bc1b43df7fa2c526a10e82b32
Johan Benschop <joh739@gmail.com>
2014-06-17 18:25:40 +0200

    Added SignalR to the IGB page.

--- CISTAR/Scripts/IGB.ts
+++ CISTAR/Scripts/IGB.ts
@@ -6,4 +6,14 @@ $(document).ready(() => {
     // The IGB will not refresh when trust is given.
     var pathname = "http://localhost:52288/";
     CCPEVE.requestTrust(pathname);
+
+
+	var hubProxy = $.connection.contosoChatHub;
+	hubProxy.client.addContosoChatMessageToPage = function (name, message) {
+		console.log(name + ' ' + message);
+	};
+
+	$.connection.hub.start().done(function () {
+		
+	});
 });
